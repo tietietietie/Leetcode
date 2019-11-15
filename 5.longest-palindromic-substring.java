4503150ee1ade1c0d99f2b525fd3ea70fab705ed
zhangtie <zhangtiezhangtie@outlook.com>
2019-11-15 16:40:21 +0800

    Endindex is exclusive

--- 5.longest-palindromic-substring.java
+++ 5.longest-palindromic-substring.java
@@ -5,37 +5,25 @@
  */
 
 // @lc code=start
-class Solution 
-{
+class Solution {
     public String longestPalindrome(String s) {
         int pleft = 0, pright = 0, maxlen = 0;
-        for(int i = 0; i < s.length(); i++)
-            for(int j = i+1; j < s.length(); j++)
-            {
-                if(isPalindrome(s, i, j) && j-i+1 > maxlen)
-                {
-                    maxlen = j-i+1;
+        for (int i = 0; i < s.length(); i++)
+            for (int j = i + 1; j < s.length(); j++) {
+                if (isPalindrome(s, i, j) && j - i + 1 > maxlen) {
+                    maxlen = j - i + 1;
                     pleft = i;
                     pright = j;
                 }
             }
-        return s.substring(pleft,pright);
-        }
-    
-    
-        private boolean isPalindrome(String s,int left, int right){
-            for(int i = 0; i <= (right -left + 1)/2; i++)
-                if(s.charAt(left+i) != s.charAt(right-i))
-                    return false;
-            return true;
-        }
-    
-    
-        public static void main(String[] args) {
-            String a = "bababa";
-            Solution s = new Solution();
-            System.out.print(s.longestPalindrome(a));
-        }
+        return s.substring(pleft, prigh+1);
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        for (int i = 0; i <= (right - left + 1) / 2; i++)
+            if (s.charAt(left + i) != s.charAt(right - i))
+                return false;
+        return true;
+    }
 }
 // @lc code=end
-
