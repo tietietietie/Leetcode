80ba4ac968b97ba13a4509a76db974a1d379e4f7
zhangtie <zhangtiezhangtie@outlook.com>
2019-11-15 17:26:15 +0800

    fix bugs

---
+++ Solutions/Leetcode5_brutalforce.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if(s.length() == 0)
+            return "";
+        int pleft = 0, pright = 0, maxlen = 0;
+        for (int i = 0; i < s.length(); i++)
+            for (int j = i + 1; j < s.length(); j++) {
+                if (isPalindrome(s, i, j) && j - i + 1 > maxlen) {
+                    maxlen = j - i + 1;
+                    pleft = i;
+                    pright = j;
+                }
+            }
+
+        return s.substring(pleft, pright+1);
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        for (int i = 0; i <= (right - left + 1) / 2; i++)
+            if (s.charAt(left + i) != s.charAt(right - i))
+                return false;
+        return true;
+    }
+}
