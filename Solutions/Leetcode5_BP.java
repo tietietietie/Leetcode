bb1fdfb6bf307909ff7bc6a54ee19646cffe5ad7
ZhangTie <zhangtie@hust.edu.cn>
2019-11-18 22:55:30 +0800

    solution保存

---
+++ Solutions/Leetcode5_BP.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length(),maxlen = 0,pleft = 0,pright = 0;
+        if(n == 0)
+            return "";
+        boolean[][] p = new boolean[n][n];
+        for(int len = 1; len <= n ;len++)
+            for(int start = 0; start < n; start++)
+            {
+                int end = start + len - 1;
+                if(end >= n)
+                    break;
+                p[start][end] = (len == 1 || len == 2 || p[start+1][end-1]) && s.charAt(start) == s.charAt(end);
+                if(p[start][end]&&(len > maxlen))
+                {
+                    pleft = start;
+                    pright = end;
+                }
+            }
+        return s.substring(pleft,pright+1);
+            }   
+    public static void main(String[] args) {
+        String a = "bababa";
+        Solution s = new Solution();
+        System.out.print(s.longestPalindrome(a));
+    }
+}
