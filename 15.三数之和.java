8f61baafe3650a8cf95fde5186eab67d5b0b265c
zhangtie <zhangtiezhangtie@outlook.com>
2019-12-10 21:00:30 +0800

    Leetcode 15 降低复杂度至n^2logn

--- 15.三数之和.java
+++ 15.三数之和.java
@@ -7,28 +7,31 @@
 // @lc code=start
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);  //不需要多余的a
-        //int[][] ans = new int[][];
+        Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList();
+        int min,mid,max,tar;
         for(int i = 0; i < nums.length-2; i++)
         {
-            //while(i != 0 && nums[i] == nums[i-1] && )
-            //    i++;
             if(i != 0 && nums[i] == nums[i-1])
                 continue;
             for(int j = i+1; j < nums.length-1; j++)
             {
-                //while(j != i+1 && nums[j] == nums[j-1])
-                //    j++;
                 if(j != i+1 && nums[j] == nums[j-1])
                     continue;
-                for(int k = j+1; k < nums.length; k++)
+                min = j+1; 
+                max  = nums.length-1;
+                tar = -(nums[i] + nums[j]);
+                while(min <= max)
                 {
-                    if(nums[i] + nums[j] + nums[k] == 0)
+                    mid = (min+max)/2;
+                    if(nums[mid] == tar)
                     {
-                        ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                        ans.add(Arrays.asList(nums[i],nums[j],nums[mid]));
                         break;
                     }
+                    else if(nums[mid] < tar)
+                        min = mid + 1;
+                    else max = mid - 1;
                 }
             }
         }
