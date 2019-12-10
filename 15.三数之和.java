c6802d786b8bbbbd077945c7556fd51f19c43fdc
zhangtie <zhangtiezhangtie@outlook.com>
2019-12-10 21:47:33 +0800

    第15题（未完成）

--- 15.三数之和.java
+++ 15.三数之和.java
@@ -9,30 +9,41 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList();
-        int min,mid,max,tar;
-        for(int i = 0; i < nums.length-2; i++)
+        int left,mid,right;
+        for(mid = 1; i < nums.length-2; mid++)
         {
-            if(i != 0 && nums[i] == nums[i-1])
-                continue;
-            for(int j = i+1; j < nums.length-1; j++)
+            if(nums[mid] == nums[mid-1] && (mid - 2 == -1 || nums[mid] != nums[mid-2]))
             {
-                if(j != i+1 && nums[j] == nums[j-1])
-                    continue;
-                min = j+1; 
-                max  = nums.length-1;
-                tar = -(nums[i] + nums[j]);
-                while(min <= max)
-                {
-                    mid = (min+max)/2;
-                    if(nums[mid] == tar)
-                    {
-                        ans.add(Arrays.asList(nums[i],nums[j],nums[mid]));
-                        break;
-                    }
-                    else if(nums[mid] < tar)
-                        min = mid + 1;
-                    else max = mid - 1;
-                }
+                int tar = -2*nums[mid];
+            }
+            left = mid-1;
+            right = mid+1;
+            while(left >= 0 && right < nums.length)
+            {
+                if()
+            }
+
+            // if(i != 0 && nums[i] == nums[i-1])
+            //     continue;
+            // for(int j = i+1; j < nums.length-1; j++)
+            // {
+            //     if(j != i+1 && nums[j] == nums[j-1])
+            //         continue;
+            //     min = j+1; 
+            //     max  = nums.length-1;
+            //     tar = -(nums[i] + nums[j]);
+            //     while(min <= max)
+            //     {
+            //         mid = (min+max)/2;
+            //         if(nums[mid] == tar)
+            //         {
+            //             ans.add(Arrays.asList(nums[i],nums[j],nums[mid]));
+            //             break;
+            //         }
+            //         else if(nums[mid] < tar)
+            //             min = mid + 1;
+            //         else max = mid - 1;
+            //     }
             }
         }
         return ans;
