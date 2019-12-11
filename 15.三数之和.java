1350a9e781a7926cc391316f89b14ebfe05db7df
zhangtie <zhangtiezhangtie@outlook.com>
2019-12-11 21:44:58 +0800

    三数之和 有序数组左右指针法

--- 15.三数之和.java
+++ 15.三数之和.java
@@ -9,41 +9,34 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList();
-        int left,mid,right;
-        for(mid = 1; i < nums.length-2; mid++)
+        int left, right;
+        for(int i = 0; i < nums.length-2 && nums[i] <= 0; i++)
         {
-            if(nums[mid] == nums[mid-1] && (mid - 2 == -1 || nums[mid] != nums[mid-2]))
+            if(i != 0 && nums[i] == nums[i-1])
+                continue;
+            left = i+1;
+            right = nums.length-1;
+            while(left < right)
             {
-                int tar = -2*nums[mid];
-            }
-            left = mid-1;
-            right = mid+1;
-            while(left >= 0 && right < nums.length)
-            {
-                if()
-            }
-
-            // if(i != 0 && nums[i] == nums[i-1])
-            //     continue;
-            // for(int j = i+1; j < nums.length-1; j++)
-            // {
-            //     if(j != i+1 && nums[j] == nums[j-1])
-            //         continue;
-            //     min = j+1; 
-            //     max  = nums.length-1;
-            //     tar = -(nums[i] + nums[j]);
-            //     while(min <= max)
-            //     {
-            //         mid = (min+max)/2;
-            //         if(nums[mid] == tar)
-            //         {
-            //             ans.add(Arrays.asList(nums[i],nums[j],nums[mid]));
-            //             break;
-            //         }
-            //         else if(nums[mid] < tar)
-            //             min = mid + 1;
-            //         else max = mid - 1;
-            //     }
+                if(left != i+1 && nums[left-1] == nums[left])
+                {
+                    left++;
+                    continue;
+                }
+                if(right != nums.length-1 && nums[right+1] == nums[right])
+                {
+                    right--;
+                    continue;
+                }
+                if(nums[left] + nums[right] + nums[i] < 0)
+                    left++;
+                else if(nums[left] + nums[right] + nums[i] > 0)
+                    right--;
+                else       
+                {
+                    ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    left++;
+                }         
             }
         }
         return ans;
