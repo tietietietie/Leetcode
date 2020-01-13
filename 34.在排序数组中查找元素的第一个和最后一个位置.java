37adb378fcb0172aba5d104ff511dd569198fbbb
ZhangTie <zhangtie@hust.edu.cn>
2020-01-13 16:42:11 +0800

    34题 二分法扩展

--- 34.在排序数组中查找元素的第一个和最后一个位置.java
+++ 34.在排序数组中查找元素的第一个和最后一个位置.java
@@ -6,8 +6,28 @@
 
 // @lc code=start
 class Solution {
+    private int targetBoundary(int[] nums, int target, boolean left){
+        int lo = 0, hi = nums.length -1;
+        while(lo <= hi){
+            int mid = (lo + hi)/2;
+            if(nums[mid] > target || (nums[mid] == target && left))
+                hi = mid - 1;
+            else
+                lo = mid + 1;
+        }
+        return lo;
+    }
+
     public int[] searchRange(int[] nums, int target) {
-        
+        int[] ans = {-1,-1};
+        int leftIdx = targetBoundary(nums,target,true);
+        int rightIdx = targetBoundary(nums,target,false)-1;
+        if(leftIdx == nums.length || nums[leftIdx] != target)
+            return ans;
+        //ans = {leftIdx,rightIdx};
+        ans[0] = leftIdx;
+        ans[1] = rightIdx;
+        return ans;
     }
 }
 // @lc code=end
