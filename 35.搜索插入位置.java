03cf1ce0045e814f7211512472894589b25e7910
ZhangTie <zhangtie@hust.edu.cn>
2020-01-13 17:39:24 +0800

    35 lo==hi情况单独拿出来讨论

--- 35.搜索插入位置.java
+++ 35.搜索插入位置.java
@@ -7,7 +7,22 @@
 // @lc code=start
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        
+        if(nums.length == 0)
+            return 0;
+        int lo = 0, hi = nums.length-1;
+        while(lo < hi){
+            int mid = (lo+hi)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                lo = mid+1;
+            else
+                hi = mid-1;
+        }
+        if(nums[lo] >= target)
+            return lo;
+        else 
+            return lo+1;
     }
 }
 // @lc code=end
