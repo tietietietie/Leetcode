3c8306f43ef73fd5a5086f746c535599b0b86088
ZhangTie <zhangtie@hust.edu.cn>
2020-01-17 14:34:45 +0800

    return递归法

--- 19.删除链表的倒数第n个节点.java
+++ 19.删除链表的倒数第n个节点.java
@@ -15,18 +15,15 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode left = dummy, right = dummy;
-        for(int i = 0; i < n; i++)
-            right = right.next;
-        while(right.next != null){
-            right = right.next;
-            left = left.next;
-        }
-        left.next = left.next.next;
-        return dummy.next;
-    }
-}
+         return removeNode(head,n)==n?head.next:head;
+     }	
+     public int removeNode(ListNode node,int n) {		
+         if(node.next == null)  return 1;
+         int m = removeNode(node.next, n);
+         if(m == n) 
+             node.next = node.next.next;
+         return m+1;
+     }
+ }
 // @lc code=end
 
