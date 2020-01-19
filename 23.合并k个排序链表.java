43d5690ef7616608ef08ba0a26ad69a3bedfc02b
ZhangTie <zhangtie@hust.edu.cn>
2020-01-19 11:18:28 +0800

    使用PQ和自定义的比较器

--- 23.合并k个排序链表.java
+++ 23.合并k个排序链表.java
@@ -15,29 +15,33 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        int index = findSmallestNode(lists);
-        if(index == -1)
-            return null;
-        else{
-            ListNode pre = lists[index];
-            lists[index] = lists[index].next;
-            pre.next = mergeKLists(lists);
-            return pre;
-        }
-    }
+        //定义ListNode的外部比较器
+        Comparator<ListNode> cmp = new ListNodeComparator();
 
-    private int findSmallestNode(ListNode[] lists){
-        int sval = Integer.MAX_VALUE;
-        int index = -1;
-        for(int i = 0; i < lists.length; i++){
-            if(lists[i] == null)
-                continue;
-            else if(lists[i].val < sval){
-                sval = lists[i].val;
-                index = i;
-            }
+        //建立优先队列
+        Queue<ListNode> pq = new PriorityQueue<ListNode>(cmp);
+        for(ListNode l : lists){
+            if(l != null)
+                pq.add(l);
         }
-        return index;
+
+        //合并链表
+        ListNode head = new ListNode(0);
+        ListNode pre = head;
+        while(!pq.isEmpty()){
+            pre.next = pq.poll();
+            ListNode next = pre.next.next;
+            if(next != null)
+                pq.add(next);
+            pre = pre.next;
+        }
+
+        return head.next;
+    }
+}
+public class ListNodeComparator implements Comparator<ListNode>{
+    public int compare(ListNode o1, ListNode o2){
+        return o1.val - o2.val;
     }
 }
 // @lc code=end
