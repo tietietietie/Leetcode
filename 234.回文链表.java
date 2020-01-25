e37e37f80b229edcea5c8b5a318d4831109fe41d
ZhangTie <zhangtie@hust.edu.cn>
2020-01-25 16:13:55 +0800

    将链表折半，后半截反转然后比较

--- 234.回文链表.java
+++ 234.回文链表.java
@@ -14,18 +14,37 @@
  * }
  */
 class Solution {
-    private ListNode frontPointer;
     public boolean isPalindrome(ListNode head) {
-        frontPointer = head;
-        return recursivelyCheck(head);
-    }
-    private boolean recursivelyCheck(ListNode currentNode){
-        if(currentNode != null){
-            if(!recursivelyCheck(currentNode.next)) return false;
-            if(currentNode.val != frontPointer.val) return false;
-            frontPointer = frontPointer.next;
+        if(head == null || head.next == null)
+            return true;
+        //折半并反转后半部分链表（快慢指针法）
+        ListNode slow = head, fast = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode halfRHead = reverse(slow.next);
+        //比较两链表
+        ListNode cmp1 = halfRHead, cmp2 = head;
+        while(cmp1 != null){
+            if(cmp1.val != cmp2.val)
+                return false;
+            cmp1 = cmp1.next;
+            cmp2 = cmp2.next;
         }
         return true;
+    }
+
+    private ListNode reverse(ListNode head){
+        ListNode pre = null;
+        ListNode next = null;
+        while(head != null){
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
     }
 }
 // @lc code=end
