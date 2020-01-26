135aed0f50ed53f1c34d9dc6c08e44dcc90f3480
ZhangTie <zhangtie@hust.edu.cn>
2020-01-26 15:16:22 +0800

    BFS

--- 104.二叉树的最大深度.java
+++ 104.二叉树的最大深度.java
@@ -20,8 +20,19 @@ class Solution {
         if(root == null){
             return 0;
         }
-        else{
-            depth = Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 0;
+        while(!queue.isEmpty()){
+            depth++;
+            int levelSize = queue.size();
+            for(int i = 0; i < levelSize; i++){
+                TreeNode cur = queue.poll();
+                if(cur.left != null)
+                    queue.offer(cur.left);
+                if(cur.right != null)
+                    queue.offer(cur.right);
+            }
         }
         return depth;
     }
