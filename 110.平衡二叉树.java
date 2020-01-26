b00dfe103ac6102d368a5382cac3ebd6503fb900
ZhangTie <zhangtie@hust.edu.cn>
2020-01-26 19:54:43 +0800

    打算用map来存每个节点和高度，但Null节点没办法存，会报错

---
+++ 110.平衡二叉树.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=java
+ *
+ * [110] 平衡二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private Map<TreeNode,Integer> map = new HashMap<>();
+    public boolean isBalanced(TreeNode root) {
+        maxDepth(root);
+        return recursivelyCheck(root);        
+    }
+    private int maxDepth(TreeNode root){
+        if(root == null){
+            map.put(root,0);
+            return 0;
+        }
+        int depth = Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
+        map.put(root,depth);
+        return depth;            
+    }
+    private boolean recursivelyCheck(TreeNode root){
+        if(Math.abs(map.get(root.left)-map.get(root.right)) > 1) return false;
+        if(!recursivelyCheck(root.left)) return false;
+        if(!recursivelyCheck(root.right)) return false;
+        return true;
+    }
+}
+// @lc code=end
+
