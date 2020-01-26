64abbe2ff67287777ea607bb202626540d0cd46b
ZhangTie <zhangtie@hust.edu.cn>
2020-01-26 21:10:19 +0800

    前序dfs，如果不满足条件返回-1，满足条件返回节点高度

--- 110.平衡二叉树.java
+++ 110.平衡二叉树.java
@@ -16,15 +16,16 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root == null)
-            return true;
-        return Math.abs(depth(root.left) - depth(root.right)) < 2 && isBalanced(root.left) && isBalanced(root.right);
+        return depth(root) == -1?false:true;
     }
 
-    public int depth(TreeNode root){
-        if(root == null)
-            return 0;
-        return Math.max(depth(root.left),depth(root.right)) + 1;
+    private int depth(TreeNode root){
+        if(root == null) return 0;
+        int left = depth(root.left);
+        if(left == -1) return -1;
+        int right = depth(root.right);
+        if(right == -1) return -1;
+        return Math.abs(left-right) < 2?Math.max(left,right)+1:-1;
     }
 }
 // @lc code=end
