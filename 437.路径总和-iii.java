2a8620a2c832368804cd9e5755c0208cc58ecdec
ZhangTie <zhangtie@hust.edu.cn>
2020-01-27 21:04:02 +0800

    使用数组保存其所有父节点

--- 437.路径总和-iii.java
+++ 437.路径总和-iii.java
@@ -16,31 +16,25 @@
  */
 class Solution {
     public int pathSum(TreeNode root, int sum) {
-        return pathSum(root, sum, new int[1000], 0);
+    int[] pathNodes = new int[1000];
+    return pathSum(root,sum,pathNodes,0);
     }
 
-    public int pathSum(TreeNode root, int sum, int[] array/*保存路径*/, int p/*指向路径终点*/) {
-        if (root == null) {
+    private int pathSum(TreeNode node, int sum, int[] pathNodes, int depth){
+        if(node == null)
             return 0;
-        }
-        int tmp = root.val;
-        int n = root.val == sum ? 1 : 0;
-        for (int i = p - 1; i >= 0; i--) {
-            tmp += array[i];
-            if (tmp == sum) {
+        int n = 0;
+        pathNodes[depth] = node.val;
+        int temp = 0;
+        for(int i = depth; i >=0; i--){
+            temp += pathNodes[i];
+            if(temp == sum)
                 n++;
-            }
         }
-        array[p] = root.val;
-        int n1 = pathSum(root.left, sum, array, p + 1);
-        int n2 = pathSum(root.right, sum, array, p + 1);
-        return n + n1 + n2;
+        int n1 = pathSum(node.left,sum,pathNodes,depth+1);
+        int n2 = pathSum(node.right,sum,pathNodes,depth+1);
+        return n+n1+n2;
     }
-
-// 作者：xiao-chao-wang-yi-lang
-// 链接：https://leetcode-cn.com/problems/path-sum-iii/solution/javajie-fa-shi-jian-100-kong-jian-93-by-xiao-chao-/
-// 来源：力扣（LeetCode）
-// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 }
 // @lc code=end
 
