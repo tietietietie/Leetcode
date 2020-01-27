70ab07599f86ab73dd7f39fc481386e39534a2a1
ZhangTie <zhangtie@hust.edu.cn>
2020-01-27 16:27:29 +0800

    使用stack DFS遍历二叉树

--- 112.路径总和.java
+++ 112.路径总和.java
@@ -14,11 +14,21 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+import javafx.util.Pair;
 class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
         if(root == null) return false;
-        if(root.val == sum && root.left == null && root.right == null) return true;
-        return hasPathSum(root.left,sum-root.val) || hasPathSum(root.right,sum-root.val);
+        Stack<Pair<TreeNode,Integer>> stack = new Stack<>();
+        stack.push(new Pair<>(root,sum));
+        while(!stack.isEmpty()){
+            Pair<TreeNode,Integer> pair = stack.pop();
+            if(pair.getKey().left == null && pair.getKey().right == null && pair.getKey().val == pair.getValue()) return true;
+            if(pair.getKey().left != null)
+                stack.push(new Pair<>(pair.getKey().left,pair.getValue()-pair.getKey().val));
+            if(pair.getKey().right != null)
+                stack.push(new Pair<>(pair.getKey().right,pair.getValue()-pair.getKey().val));
+        }
+        return false;
     }
 }
 // @lc code=end
