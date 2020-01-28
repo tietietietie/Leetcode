196e617d4e475be5e8e646bd67f2767ec3647638
ZhangTie <zhangtie@hust.edu.cn>
2020-01-28 13:42:30 +0800

    递归法，双树镜像问题

--- 101.对称二叉树.java
+++ 101.对称二叉树.java
@@ -18,21 +18,13 @@ class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root == null)
             return true;
-        LinkedList<TreeNode> queue = new LinkedList<>();
-        queue.offer(root.left);
-        queue.offer(root.right);
-        while(!queue.isEmpty()){
-            TreeNode n1 = queue.poll();
-            TreeNode n2 = queue.poll();
-            if(n1 == null && n2 == null) continue;
-            if(n1 == null || n2 == null) return false;
-            if(n1.val != n2.val) return false;
-            queue.add(n1.left);
-            queue.add(n2.right);
-            queue.add(n1.right);
-            queue.add(n2.left);
-        }
-        return true;
+        return isMirror(root.left,root.right);
+    }
+
+    private boolean isMirror(TreeNode n1, TreeNode n2){
+        if(n1 == null && n2 == null) return true;
+        if(n1 == null || n2 == null) return false;
+        return n1.val == n2.val && isMirror(n1.left,n2.right) && isMirror(n1.right,n2.left);
     }
 }
 // @lc code=end
