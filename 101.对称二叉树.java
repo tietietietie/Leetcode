fcbf87443bda7e11776070a49216b4a3da1f5e41
ZhangTie <zhangtie@hust.edu.cn>
2020-01-28 13:23:03 +0800

    用queue实现BFS，改变入栈顺序，即可以让出栈时两节点刚好为我们要比较的节点

--- 101.对称二叉树.java
+++ 101.对称二叉树.java
@@ -16,33 +16,23 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        //array存每一层节点
-        TreeNode[] lNodes = new TreeNode[1];
-        lNodes[0] = root;
-        return isSymmetric(lNodes,0);
-    }
-
-    private boolean isSymmetric(TreeNode[] lNodes, int level){
-        //判断当前层是否对称
-        int nullCount = 0;
-        for(TreeNode n:lNodes){
-            if(n == null)
-                nullCount++;
-        }
-        if (nullCount == 1<<level)
+        if(root == null)
             return true;
-        int i = 0, j = (1<<level) - 1;
-        while(i < j){
-            if((lNodes[i] == null && lNodes[j] != null) || (lNodes[j] == null && lNodes[i] != null)||lNodes[i].val != lNodes[j].val)
-                return false;
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+        while(!queue.isEmpty()){
+            TreeNode n1 = queue.poll();
+            TreeNode n2 = queue.poll();
+            if(n1 == null && n2 == null) continue;
+            if(n1 == null || n2 == null) return false;
+            if(n1.val != n2.val) return false;
+            queue.add(n1.left);
+            queue.add(n2.right);
+            queue.add(n1.right);
+            queue.add(n2.left);
         }
-        //更新lNodes到下一层
-        TreeNode[] nextNodes = new TreeNode[1<<(level+1)];
-        for(int k = 0; k < 1<<level; k++){
-            nextNodes[2*k] = lNodes[k].left;
-            nextNodes[2*k+1] = lNodes[k].right;
-        }
-        return isSymmetric(nextNodes,level+1);
+        return true;
     }
 }
 // @lc code=end
