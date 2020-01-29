37f6f03f21b5f1a0960d8840301565641505d4f9
ZhangTie <zhangtie@hust.edu.cn>
2020-01-29 22:34:52 +0800

    备忘录版DP

--- 337.打家劫舍-iii.java
+++ 337.打家劫舍-iii.java
@@ -15,14 +15,17 @@
  * }
  */
 class Solution {
+    private Map<TreeNode,Integer> memo = new HashMap<>();
     public int rob(TreeNode root) {
         if(root == null) return 0;
         //val1表示打劫当前节点能获得的最大财富
+        if(memo.containsKey(root)) return memo.get(root);
         int val1 = root.val;
         if(root.left != null) val1 += (rob(root.left.left) + rob(root.left.right));
         if(root.right != null) val1 += (rob(root.right.left) + rob(root.right.right));
         //val2表示不打劫当前节点能获得的最大财富
         int val2 = rob(root.right) + rob(root.left);
+        memo.put(root,Math.max(val1,val2));
         return Math.max(val1,val2);      
     }
 }
