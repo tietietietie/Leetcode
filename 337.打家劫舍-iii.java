7263dbe2bf0a43d36dbed773ed37fdb68ea43920
ZhangTie <zhangtie@hust.edu.cn>
2020-01-29 22:54:49 +0800

    改变了递归函数的定义，也就是状态传递函数

--- 337.打家劫舍-iii.java
+++ 337.打家劫舍-iii.java
@@ -15,18 +15,24 @@
  * }
  */
 class Solution {
-    private Map<TreeNode,Integer> memo = new HashMap<>();
     public int rob(TreeNode root) {
-        if(root == null) return 0;
-        //val1表示打劫当前节点能获得的最大财富
-        if(memo.containsKey(root)) return memo.get(root);
-        int val1 = root.val;
-        if(root.left != null) val1 += (rob(root.left.left) + rob(root.left.right));
-        if(root.right != null) val1 += (rob(root.right.left) + rob(root.right.right));
-        //val2表示不打劫当前节点能获得的最大财富
-        int val2 = rob(root.right) + rob(root.left);
-        memo.put(root,Math.max(val1,val2));
-        return Math.max(val1,val2);      
+        //results里面保存着打劫root节点，和不打劫root节点两种选择，
+        //能在root子树中得到的财产
+        int[] results = robOptions(root);
+        return Math.max(results[0],results[1]);     
+    }
+
+    private int[] robOptions(TreeNode root){
+        if(root == null) return new int[2];
+        int[] left = robOptions(root.left);
+        int[] right = robOptions(root.right);
+        //当前节点选择rob,则其能打劫到的财产为本节点财产加上
+        //左子节点不被打劫时的左子树的最大财产，和右子节点不被打劫
+        //时右子树的最大财产
+        int rob = root.val + left[0] + right[0];
+        //当前节点不被打劫，则返回左右子树能打劫的最大财产和
+        int not_rob = Math.max(left[0],left[1]) + Math.max(right[0],right[1]);
+        return new int[]{not_rob,rob};
     }
 }
 // @lc code=end
