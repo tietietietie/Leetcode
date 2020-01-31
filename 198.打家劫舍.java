94d16c865dad6bacb7ae71191c50f6c3342a9213
ZhangTie <zhangtie@hust.edu.cn>
2020-01-31 10:17:03 +0800

    合并一个If

--- 198.打家劫舍.java
+++ 198.打家劫舍.java
@@ -10,7 +10,6 @@ class Solution {
         //n表示户数
         int n = nums.length;
         if(n == 0) return 0;
-        if(n == 1) return nums[0];
         //temp存储上一节点偷了或没偷，能获得的最大财产
         int[] temp = new int[]{0,nums[0]};
         //迭代遍历到第n个节点
