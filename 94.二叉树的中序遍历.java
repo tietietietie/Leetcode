0c5f1e0540b282a654a30f15c0a255fae27545ad
ZhangTie <zhangtie@hust.edu.cn>
2020-02-02 11:19:25 +0800

    （迭代法）用栈实现递归

--- 94.二叉树的中序遍历.java
+++ 94.二叉树的中序遍历.java
@@ -15,12 +15,21 @@
  * }
  */
 class Solution {
-    private List<Integer> ans = new ArrayList<>();
     public List<Integer> inorderTraversal(TreeNode root) {
-        if(root == null) return ans;
-        inorderTraversal(root.left);
-        ans.add(root.val);
-        inorderTraversal(root.right);
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        //cur表示当前遍历的子树
+        TreeNode cur = root;
+        while(cur != null || !stack.isEmpty()){
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            //cur此时定位到此次循环，也就是之前cur的最左叶子节点的左侧Null,pop最左侧节点
+            cur = stack.pop();//此时表示cur左子树遍历完成
+            ans.add(cur.val); //添加根节点
+            cur = cur.right; //定位到根节点右子树
+        }
         return ans;
     }
 }
