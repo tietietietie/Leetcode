8104ebb8937844abba718c2feb02e7c2a33ab34b
ZhangTie <zhangtie@hust.edu.cn>
2020-02-02 19:27:22 +0800

    Morris解法

--- 94.二叉树的中序遍历.java
+++ 94.二叉树的中序遍历.java
@@ -17,18 +17,27 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        //cur表示当前遍历的子树
         TreeNode cur = root;
-        while(cur != null || !stack.isEmpty()){
-            while(cur != null){
-                stack.push(cur);
-                cur = cur.left;
+        while(cur != null){
+            //如果当前cur树没有左子树，不做任何变化
+            if(cur.left == null){
+                ans.add(cur.val);
+                cur = cur.right;
+                continue;
             }
-            //cur此时定位到此次循环，也就是之前cur的最左叶子节点的左侧Null,pop最左侧节点
-            cur = stack.pop();//此时表示cur左子树遍历完成
-            ans.add(cur.val); //添加根节点
-            cur = cur.right; //定位到根节点右子树
+            else{
+                TreeNode pre = cur.left;
+                //找到前驱节点：左子树最右节点
+                while(pre.right != null){
+                    pre = pre.right;
+                }
+                //连接
+                pre.right = cur;
+                //防止成环
+                TreeNode temp = cur;
+                cur = cur.left;
+                temp.left = null;
+            }
         }
         return ans;
     }
