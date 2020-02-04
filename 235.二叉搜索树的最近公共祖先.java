4a4aee0e325a894a3866dfa01b57c4683bfadd25
ZhangTie <zhangtie@hust.edu.cn>
2020-02-04 11:25:21 +0800

    每个节点递归遍历版

--- 235.二叉搜索树的最近公共祖先.java
+++ 235.二叉搜索树的最近公共祖先.java
@@ -16,28 +16,11 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        LinkedList<TreeNode> q1 = new LinkedList<>();
-        LinkedList<TreeNode> q2 = new LinkedList<>(); 
-        ancestors(root,p,q1);
-        ancestors(root,q,q2);
-        TreeNode ans = null;
-        while(q1.peek() == q2.peek()){
-            ans = q1.peek();
-            q1.poll();
-            q2.poll();                 
-        }
-        return ans;     
-    }
-
-    private void ancestors(TreeNode root, TreeNode node, Queue<TreeNode> q){
-        q.offer(root);
-        if(root.val == node.val) 
-            return;
-        else if(root.val < node.val)
-            ancestors(root.right,node,q);
-        else
-            ancestors(root.left,node,q);
-        return;
+        if(p.val < root.val && q.val < root.val) 
+            return lowestCommonAncestor(root.left,p,q);
+        if(p.val > root.val && q.val > root.val) 
+            return lowestCommonAncestor(root.right,p,q);
+        return root;
     }
 }
 // @lc code=end
