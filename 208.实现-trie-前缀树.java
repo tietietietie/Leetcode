74059f8700613ee18068e103fa19fcf0cffc57b6
ZhangTie <zhangtie@hust.edu.cn>
2020-02-08 16:36:41 +0800

    根据前缀树的定义，实现插入，搜索，判断前缀的操作

---
+++ 208.实现-trie-前缀树.java
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=208 lang=java
+ *
+ * [208] 实现 Trie (前缀树)
+ */
+
+// @lc code=start
+class Trie {
+
+    private static int R = 26;
+    private Node root;
+
+    private static class Node{
+        private int val = 0;
+        private Node[] next = new Node[R];
+    }
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new Node();
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        insert(root,word,0);
+    }
+
+    private Node insert(Node node, String word, int d){
+        if(node == null) node = new Node();
+        if(d == word.length()) {
+            node.val = 1; 
+            return node;}
+        char c = word.charAt(d);
+        node.next[c-'a'] = insert(node.next[c-'a'],word,d+1);
+        return node;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        return search(root,word,0);                
+    }
+
+    private boolean search(Node node, String word, int d){
+        if(d == word.length() && node.val == 1) return true;
+        if(d == word.length() && node.val == 0) return false;
+        char c = word.charAt(d);
+        return node.next[c-'a'] == null?false:search(node.next[c-'a'],word,d+1);
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        return startWith(root,prefix,0);        
+    }
+
+    private boolean startWith(Node node, String word, int d){
+        if(d == word.length()) return true;
+        char c = word.charAt(d);
+        return node.next[c-'a'] == null?false:startWith(node.next[c-'a'],word,d+1);
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+// @lc code=end
+
