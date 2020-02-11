f9aa66f24692ca1d91c59f16ae3bcaa3acf52f58
ZhangTie <zhangtie@hust.edu.cn>
2020-02-11 20:59:42 +0800

    直接法，用hashmap存key/value

--- 677.键值映射.java
+++ 677.键值映射.java
@@ -7,54 +7,25 @@
 // @lc code=start
 class MapSum {
 
-    private class Node {
-        Node[] child = new Node[26];
-        int value;
-    }
-
-    private Node root = new Node();
-
+    Map<String,Integer> map;
     public MapSum() {
-
+        this.map = new HashMap<>();
     }
 
     public void insert(String key, int val) {
-        insert(key, root, val);
+        map.put(key,val);
     }
 
-    private void insert(String key, Node node, int val) {
-        if (node == null) return;
-        if (key.length() == 0) {
-            node.value = val;
-            return;
-        }
-        int index = indexForChar(key.charAt(0));
-        if (node.child[index] == null) {
-            node.child[index] = new Node();
-        }
-        insert(key.substring(1), node.child[index], val);
-    }
 
     public int sum(String prefix) {
-        return sum(prefix, root);
-    }
-
-    private int sum(String prefix, Node node) {
-        if (node == null) return 0;
-        if (prefix.length() != 0) {
-            int index = indexForChar(prefix.charAt(0));
-            return sum(prefix.substring(1), node.child[index]);
-        }
-        int sum = node.value;
-        for (Node child : node.child) {
-            sum += sum(prefix, child);
+        int sum = 0;
+        for(String key : map.keySet()){
+            if(key.startsWith(prefix))
+                sum += map.get(key);
         }
         return sum;
     }
 
-    private int indexForChar(char c) {
-        return c - 'a';
-    }
 }
 
 /**
