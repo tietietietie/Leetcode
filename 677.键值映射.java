62dde0929960eadba804d9d246a2877958f5426e
ZhangTie <zhangtie@hust.edu.cn>
2020-02-11 21:52:36 +0800

    Trie法，每个node代表一个prefix，存着sum

--- 677.键值映射.java
+++ 677.键值映射.java
@@ -6,24 +6,39 @@
 
 // @lc code=start
 class MapSum {
+    private class TireNode{
+        Map<Character,TireNode> next = new HashMap<>();
+        int sum;
+    }
 
     Map<String,Integer> map;
+    TireNode root;
+    
     public MapSum() {
         this.map = new HashMap<>();
+        this.root = new TireNode();
     }
 
     public void insert(String key, int val) {
+        int delte = val - map.getOrDefault(key,0);
         map.put(key,val);
+        TireNode cur = root;
+        cur.sum += delte;
+        for(char c : key.toCharArray()){
+            cur.next.putIfAbsent(c,new TireNode());
+            cur = cur.next.get(c);
+            cur.sum += delte;
+        }
     }
 
 
     public int sum(String prefix) {
-        int sum = 0;
-        for(String key : map.keySet()){
-            if(key.startsWith(prefix))
-                sum += map.get(key);
+        TireNode cur = root;
+        for(char c : prefix.toCharArray()){
+            cur = cur.next.get(c);
+            if(cur == null) return 0;
         }
-        return sum;
+        return cur.sum;
     }
 
 }
