2cd0324d70bbbdd91121dc7823947749bf85bad6
ZhangTie <zhangtie@hust.edu.cn>
2020-02-13 19:54:27 +0800

    插入排序：把链表分成三个部分

---
+++ 147.对链表进行插入排序.java
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=147 lang=java
+ *
+ * [147] 对链表进行插入排序
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        //设置哑节点
+        ListNode dummy = new ListNode(Integer.MIN_VALUE);
+        dummy.next = head;
+        ListNode cur1 = head;
+        ListNode pre = dummy;
+        while(cur1 != null){
+            ListNode next = cur1.next;
+            if(cur1.val >= pre.val){
+                pre = cur1;
+                cur1 = next;
+                continue;
+            }
+            else{
+                ListNode cur2 = dummy;
+                while(cur2 != pre){
+                    if(cur2.val <= cur1.val && cur2.next.val > cur1.val){
+                        ListNode temp = cur2.next;
+                        cur2.next = cur1;
+                        cur1.next = temp;
+                        pre.next = next;
+                        cur1 = next;
+                        break;
+                    }
+                    cur2 = cur2.next;
+                }
+            }
+        }
+        return dummy.next;
+    }
+}
+// @lc code=end
+
