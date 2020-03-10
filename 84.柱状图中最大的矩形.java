cd3d839dbba91b0f96a96ab2468a4f61e9969177
ZhangTie <zhangtie@hust.edu.cn>
2020-03-10 19:47:53 +0800

    左右边界法改进：数组

--- 84.柱状图中最大的矩形.java
+++ 84.柱状图中最大的矩形.java
@@ -10,14 +10,22 @@ class Solution {
         int length = heights.length;
         if(length == 0) return 0;
         int ans = 0;
+        int[] minLefts = new int[length];
+        int[] minRights = new int[length];
         for(int i = 0; i < length; i++){
-            int curHeight = heights[i];
-            int l = i-1, r = i+1;
-            while(r < length && heights[r] >= curHeight)
-                r++;
-            while(l >= 0 && heights[l] >= curHeight)
-                l--;
-            ans = Math.max(ans,(r-l-1)*curHeight);
+            int l = i-1;
+            while(l >= 0 && heights[l] >= heights[i])
+                l = minLefts[l];
+            minLefts[i] = l;
+        }
+        for(int i = length-1; i >= 0; i--){
+            int r = i+1;
+            while(r < length && heights[r] >= heights[i])
+                r = minRights[r];
+            minRights[i] = r;
+        }
+        for(int i = 0; i < length; i++){
+            ans = Math.max(ans,(minRights[i]-minLefts[i]-1)*heights[i]);
         }
         return ans;
     }
