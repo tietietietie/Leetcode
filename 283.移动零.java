5c4ce251df66f0918a784c70f92d40a581fae692
ZhangTie <zhangtie@hust.edu.cn>
2020-03-10 21:58:30 +0800

    移动0，双指针

---
+++ 283.移动零.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=java
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int length = nums.length;
+        int l = -1, cur = 0;
+        while(cur < length && nums[cur] != 0)
+            cur++;
+        l = cur;
+        cur++;
+        while(cur < length){
+            if(nums[cur] == 0)
+                cur++;
+            else{
+                swap(nums,l,cur);
+                l++;
+                cur++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int l, int cur){
+        int temp = nums[l];
+        nums[l] = nums[cur];
+        nums[cur] = temp; 
+    }
+}
+// @lc code=end
+
