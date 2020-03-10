46a257f02e7d35aa789b45391342ad16e3f91467
ZhangTie <zhangtie@hust.edu.cn>
2020-03-10 20:59:16 +0800

    左右边界法：单调栈

--- 84.柱状图中最大的矩形.java
+++ 84.柱状图中最大的矩形.java
@@ -10,23 +10,15 @@ class Solution {
         int length = heights.length;
         if(length == 0) return 0;
         int ans = 0;
-        int[] minLefts = new int[length];
-        int[] minRights = new int[length];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
         for(int i = 0; i < length; i++){
-            int l = i-1;
-            while(l >= 0 && heights[l] >= heights[i])
-                l = minLefts[l];
-            minLefts[i] = l;
+            while(stack.peek() != -1 && heights[stack.peek()] > heights[i])
+                ans = Math.max(ans,heights[stack.pop()]*(i - stack.peek() - 1));
+            stack.push(i);
         }
-        for(int i = length-1; i >= 0; i--){
-            int r = i+1;
-            while(r < length && heights[r] >= heights[i])
-                r = minRights[r];
-            minRights[i] = r;
-        }
-        for(int i = 0; i < length; i++){
-            ans = Math.max(ans,(minRights[i]-minLefts[i]-1)*heights[i]);
-        }
+        while(stack.peek() != -1)
+            ans = Math.max(ans,heights[stack.pop()]*(length - stack.peek() - 1));
         return ans;
     }
 }
