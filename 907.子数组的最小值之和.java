8c52a179e44ac8cbd94d7f3ea464d17795f7a2fb
ZhangTie <zhangtie@hust.edu.cn>
2020-03-11 21:04:05 +0800

    DP确定left和right

--- 907.子数组的最小值之和.java
+++ 907.子数组的最小值之和.java
@@ -8,25 +8,26 @@
 class Solution {
     public int sumSubarrayMins(int[] A) {
         int length = A.length;
-        Stack<Integer> prefix = new Stack<>();
-        Stack<Integer> suffix = new Stack<>();
         int[] left = new int[length];
         int[] right = new int[length];
-        int ans = 0;
+        left[0] = -1;
+        right[length-1] = length;
         for(int i = 0; i < length; i++){
-            while(!prefix.isEmpty() && A[prefix.peek()] > A[i])
-                prefix.pop();
-            left[i] = prefix.isEmpty()?-1:prefix.peek();
-            prefix.push(i);
+            int l = i-1;
+            while(l >=0 && A[l] > A[i])
+                l = left[l];
+            left[i] = l;
         }
         for(int i = length-1; i >= 0; i--){
-            while(!suffix.isEmpty() && A[suffix.peek()] >= A[i])
-                suffix.pop();
-            right[i] = suffix.isEmpty()?length:suffix.peek();
-            suffix.push(i);
+            int r = i+1;
+            while(r < length && A[r] >= A[i])
+                r = right[r];
+            right[i] = r;
         }
+        int ans = 0;
+        int mod = 1000000007;
         for(int i = 0; i < length; i++)
-            ans = (ans + (i-left[i])*(right[i]-i)*A[i])%1000000007;
+            ans = (ans + (i-left[i])*(right[i]-i)*A[i]) % mod;
         return ans;
     }
 }
