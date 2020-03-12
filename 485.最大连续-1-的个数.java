856013274e311831168f4591d9f7d07475ba10a1
ZhangTie <zhangtie@hust.edu.cn>
2020-03-12 17:22:03 +0800

    利用l和cur，统计连续区间

--- 485.最大连续-1-的个数.java
+++ 485.最大连续-1-的个数.java
@@ -8,17 +8,17 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         int length = nums.length;
-        int count = 0;
-        int maxCount = 0;
-        for(int i = 0; i < length; i++){
-            if(nums[i] == 1)
-                count++;
-            else{
-                maxCount = Math.max(maxCount,count);
-                count = 0;
-            }
+        int cur = 0;
+        int ans = 0;
+        while(cur < length){
+            while(cur < length && nums[cur] == 0)
+                cur++;
+            int l = cur;
+            while(cur < length && nums[cur] == 1)
+                cur++;
+            ans = Math.max(ans,cur-l);
         }
-        return Math.max(maxCount,count);
+        return ans;
     }
 }
 // @lc code=end
