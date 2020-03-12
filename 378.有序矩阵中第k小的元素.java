e5cb949ce5daf1af0516381346241eb72d157159
ZhangTie <zhangtie@hust.edu.cn>
2020-03-12 22:07:22 +0800

    二分，没利用排序矩阵性质

--- 378.有序矩阵中第k小的元素.java
+++ 378.有序矩阵中第k小的元素.java
@@ -5,33 +5,19 @@
  */
 
 // @lc code=start
-class Solution {
+public class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int n = matrix.length;
-        PriorityQueue<Tuple> pq = new PriorityQueue<>();
-        for(int i = 0; i < n; i++)
-            pq.offer(new Tuple(0,i,matrix[0][i]));
-        for(int i = 0; i < k-1; i++){
-            Tuple temp = pq.poll();
-            if(temp.x == n-1)
-                continue;
-            pq.offer(new Tuple(temp.x+1,temp.y,matrix[temp.x+1][temp.y]));
+        int lo = matrix[0][0], hi = matrix[matrix.length - 1][matrix[0].length - 1];//[lo, hi)
+        while(lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            int count = 0;
+            for(int i = 0; i < matrix.length; i++) 
+                for(int j = 0; j < matrix.length && matrix[i][j] <= mid; j++)
+                    count++;
+            if(count < k) lo = mid + 1;
+            else hi = mid;
         }
-        return pq.poll().val;
-    }
-
-    private class Tuple implements Comparable<Tuple>{
-        int x,y,val;
-        public Tuple(int x, int y, int val){
-            this.x = x;
-            this.y = y;
-            this.val = val;
-        }
-
-        @Override
-        public int compareTo(Tuple that){
-            return this.val - that.val;
-        }
+        return lo;
     }
 }
 // @lc code=end
