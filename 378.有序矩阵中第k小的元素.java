280c32907057b2395eea1937f0120dd5a47f786f
ZhangTie <zhangtie@hust.edu.cn>
2020-03-12 22:17:37 +0800

    二分法：右上角开始比较

--- 378.有序矩阵中第k小的元素.java
+++ 378.有序矩阵中第k小的元素.java
@@ -10,10 +10,11 @@ public class Solution {
         int lo = matrix[0][0], hi = matrix[matrix.length - 1][matrix[0].length - 1];//[lo, hi)
         while(lo < hi) {
             int mid = lo + (hi - lo) / 2;
-            int count = 0;
-            for(int i = 0; i < matrix.length; i++) 
-                for(int j = 0; j < matrix.length && matrix[i][j] <= mid; j++)
-                    count++;
+            int count = 0,  j = matrix[0].length - 1;
+            for(int i = 0; i < matrix.length; i++) {
+                while(j >= 0 && matrix[i][j] > mid) j--;
+                count += (j + 1);
+            }
             if(count < k) lo = mid + 1;
             else hi = mid;
         }
