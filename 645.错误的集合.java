54d7675d66f6af0893c1efb81896114bc5fe99ab
ZhangTie <zhangtie@hust.edu.cn>
2020-03-13 14:54:31 +0800

    排序后一次遍历，注意missing的特殊情况

--- 645.错误的集合.java
+++ 645.错误的集合.java
@@ -9,17 +9,16 @@ class Solution {
     public int[] findErrorNums(int[] nums) {
         Arrays.sort(nums);
         int length = nums.length;
-        int dup = 1;
-        for(; dup < length; dup++)
-            if(nums[dup] == nums[dup-1])
-                break;
-        int[] ans = new int[2];
-        ans[0] = nums[dup];
-        int curSum = 0;
-        for(int i : nums)
-            curSum += i;
-        int preSum = (length+1)*(length)/2;
-        ans[1] = ans[0] + preSum - curSum;
+        int dup = -1, missing = 1;
+        for(int i = 1; i < length; i++){
+            if(nums[i] == nums[i-1])
+                dup = nums[i];
+            else if(nums[i] > nums[i-1]+1)
+                missing = nums[i-1]+1;
+        }
+        if(nums[length-1] != length)
+            missing = length;
+        int[] ans = new int[]{dup,missing};
         return ans;
     }
 }
