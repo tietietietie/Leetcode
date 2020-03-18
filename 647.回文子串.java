fb8458d85457b3e9ebf232602f255b51492d5d9f
ZhangTie <zhangtie@hust.edu.cn>
2020-03-18 21:52:19 +0800

    中心扩展法

--- 647.回文子串.java
+++ 647.回文子串.java
@@ -9,25 +9,17 @@ class Solution {
     public int countSubstrings(String s) {
         int length = s.length();
         if(length == 0) return 0;
-        char[] chars = s.toCharArray();
-        int count = 0;
-        for(int i = 0; i < length; i++)
-            for(int j = i; j < length; j++)
-                if(isPalindrome(chars,i,j))
-                    count++;
-        return count;
-    }
-
-    private boolean isPalindrome(char[] chars, int l, int r){
-        while(l < r){
-            if(chars[l] == chars[r]){
-                l++;
-                r--;
-            }else{
-                return false;
+        int ans = 0;
+        for(int center = 0; center <= 2*length-2; center++){
+            int left = center/2;
+            int right = left + center%2;
+            while(left >= 0 && right < length && s.charAt(left) == s.charAt(right)){
+                ans++;
+                left--;
+                right++;
             }
         }
-        return true;   
+        return ans;
     }
 }
 // @lc code=end
