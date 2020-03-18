386e2d2b88aaff96b09b63459b3564b7040e6199
ZhangTie <zhangtie@hust.edu.cn>
2020-03-18 17:23:54 +0800

    扩大hashmap提高时间效率

--- 409.最长回文串.java
+++ 409.最长回文串.java
@@ -8,18 +8,11 @@
 class Solution {
     public int longestPalindrome(String s) {
         char[] chars = s.toCharArray();
-        int[] map1 = new int[26];
-        int[] map2 = new int[26];
-        for(char c : chars){
-            if(c > 'Z')
-                map1[c-'a']++;
-            else
-                map2[c-'A']++;
-        }
+        int[] map = new int[128];
+        for(char c : chars)
+            map[c]++;
         int pair = 0;
-        for(int i : map1)
-            pair += (i/2);
-        for(int i : map2)
+        for(int i : map)
             pair += (i/2);
         int ans = pair*2;
         if(ans < chars.length)
