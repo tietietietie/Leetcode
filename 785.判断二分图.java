51db87a1fd45b232ed2a085655cf84703c3a0e82
ZhangTie <zhangtie@hust.edu.cn>
2020-03-23 16:49:32 +0800

    DFS（stack版）+着色法

--- 785.判断二分图.java
+++ 785.判断二分图.java
@@ -6,43 +6,27 @@
 
 // @lc code=start
 class Solution {
-    private HashSet<Integer> A;
-    private HashSet<Integer> B;
     public boolean isBipartite(int[][] graph) {
         int length = graph.length;
-        boolean[] visited = new boolean[length];
-        A = new HashSet<>();
-        B = new HashSet<>();
+        int[] colors = new int[length];
+        //-1:no color, 0:blue, 1:red
+        Arrays.fill(colors,-1);
         for(int i = 0; i < length; i++){
-            if(visit(graph,i,visited) == false)
-                return false;
-        }
-        return true;
-    }
-
-    private boolean visit(int[][] graph, int i, boolean[] visited){
-        if(!visited[i]){
-            visited[i] = true;
-            A.add(i);
-            for(int j : graph[i]){
-                if(A.contains(j))
-                    return false;
-                visited[j] = true;
-                B.add(j);
-            }
-        }else if(A.contains(i)){
-            for(int j : graph[i]){
-                if(A.contains(j))
-                    return false;
-                visited[j] = true;
-                B.add(j);
-            }
-        }else{
-            for(int j : graph[i]){
-                if(B.contains(j))
-                    return false;
-                visited[j] = true;
-                A.add(j);
+            //如果当前i节点未被连接过，则从新开始一个新的stack，对其连接的所有节点着色
+            if(colors[i] == -1){
+                colors[i] = 0;
+                Stack<Integer> stack = new Stack<>();
+                stack.push(i);
+                while(!stack.isEmpty()){
+                    int node = stack.pop();
+                    for(int j : graph[node]){
+                        if(colors[j] == -1){
+                            colors[j] = colors[node] ^ 1;
+                            stack.push(j);
+                        }else if(colors[j] == colors[node])
+                            return false;
+                    }
+                }
             }
         }
         return true;
