190163ad6a73ef06791752de4d5d9bbda5f48d68
ZhangTie <zhangtie@hust.edu.cn>
2020-03-23 18:32:55 +0800

    无向图递归版DFS

--- 785.判断二分图.java
+++ 785.判断二分图.java
@@ -9,26 +9,22 @@ class Solution {
     public boolean isBipartite(int[][] graph) {
         int length = graph.length;
         int[] colors = new int[length];
-        //-1:no color, 0:blue, 1:red
         Arrays.fill(colors,-1);
         for(int i = 0; i < length; i++){
-            //如果当前i节点未被连接过，则从新开始一个新的stack，对其连接的所有节点着色
-            if(colors[i] == -1){
-                colors[i] = 0;
-                Stack<Integer> stack = new Stack<>();
-                stack.push(i);
-                while(!stack.isEmpty()){
-                    int node = stack.pop();
-                    for(int j : graph[node]){
-                        if(colors[j] == -1){
-                            colors[j] = colors[node] ^ 1;
-                            stack.push(j);
-                        }else if(colors[j] == colors[node])
-                            return false;
-                    }
-                }
-            }
+            if(colors[i] == -1 && !dfs(graph,i,0,colors))
+                return false;
         }
+        return true;
+    }
+
+
+    private boolean dfs(int[][] graph, int i, int suggestColor, int[] colors){
+        if(colors[i] != -1)
+            return colors[i] == suggestColor;
+        colors[i] = suggestColor;
+        for(int nei : graph[i])
+            if(!dfs(graph,nei,suggestColor ^ 1,colors))
+                return false;
         return true;
     }
 }
