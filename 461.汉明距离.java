9e36794790f0cda7058277f2ce91585603fcee16
ZhangTie <zhangtie@hust.edu.cn>
2020-03-25 20:22:51 +0800

    逐位统计1

--- 461.汉明距离.java
+++ 461.汉明距离.java
@@ -8,10 +8,12 @@
 class Solution {
     public int hammingDistance(int x, int y) {
         int xor = x ^ y;
+        int cmp = 1;
         int count = 0;
-        while(xor != 0){
-            count++;
-            xor = xor & (xor-1);
+        while(cmp != 0){
+            if((xor & cmp) != 0)
+                count++;
+            cmp = cmp << 1;
         }
         return count;
     }
