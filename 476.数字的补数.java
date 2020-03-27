17fb946b202c0786553285db9854785032681791
ZhangTie <zhangtie@hust.edu.cn>
2020-03-27 16:23:11 +0800

    不使用循环

--- 476.数字的补数.java
+++ 476.数字的补数.java
@@ -7,10 +7,12 @@
 // @lc code=start
 class Solution {
     public int findComplement(int num) {
-        if(num < 0) return -1;
-        int mask = 1 << 30;
-        while((num & mask) == 0) mask >>= 1;
-        mask = (mask << 1) - 1;
+        int mask = num;
+        mask |= mask >> 1;
+        mask |= mask >> 2;
+        mask |= mask >> 4;
+        mask |= mask >> 8;
+        mask |= mask >> 16;
         return mask ^ num;
     }
 }
