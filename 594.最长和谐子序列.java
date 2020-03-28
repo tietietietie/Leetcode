1d1fd8d18426850e855786f2b26dab3a730fb500
ZhangTie <zhangtie@hust.edu.cn>
2020-03-28 16:39:22 +0800

    hash

--- 594.最长和谐子序列.java
+++ 594.最长和谐子序列.java
@@ -7,31 +7,18 @@
 // @lc code=start
 class Solution {
     public int findLHS(int[] nums) {
-        int length = nums.length;
-        int ans = 0;
-        for(int i = 0; i < length; i++){
-            int left = findLeft(nums,i,nums[i],nums[i]+1);
-            int right = findRight(nums,i,nums[i],nums[i]+1);
-            ans = Math.max(right-left-1,ans);
-            left = findLeft(nums,i,nums[i]-1,nums[i]);
-            right = findRight(nums,i,nums[i]-1,nums[i]);
-            ans = Math.max(right-left-1,ans);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            if(map.get(num) == null)
+                map.put(num,1);
+            else
+                map.put(num,map.get(num)+1);
         }
+        int ans = 0;
+        for(int num : map.keySet())
+            if(map.get(num+1) != null)
+                ans = Math.max(ans,map.get(num)+map.get(num+1));
         return ans;
-    }
-
-    private int findLeft(int[] nums, int i, int min, int max){
-        int left = i;
-        while(left >= 0 && (nums[left] == min || nums[left] == max))
-            left--;
-         return left;   
-    }
-
-    private int findRight(int[] nums, int i, int min, int max){
-        int right = i;
-        while(right < nums.length && (nums[right] == min || nums[right] == max))
-            right++;
-        return right;
     }
 }
 // @lc code=end
