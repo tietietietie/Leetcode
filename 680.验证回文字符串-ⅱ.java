c93d95f73e5bb4e655f7e6f118391003411ba77e
ZhangTie <zhangtie@hust.edu.cn>
2020-03-30 16:48:46 +0800

    双指针+迭代

--- 680.验证回文字符串-ⅱ.java
+++ 680.验证回文字符串-ⅱ.java
@@ -7,21 +7,14 @@
 // @lc code=start
 class Solution {
     public boolean validPalindrome(String s) {
-        char[] chars = s.toCharArray();
-        int l = 0, r = chars.length-1;
-        boolean deleted = false;
-        return validPalindrome(chars,l,r,deleted);
-    }
-
-
-    private boolean validPalindrome(char[] chars, int l, int r, boolean deleted){
-        if(l >= r) return true;
-        if(chars[l] != chars[r] && deleted) return false;
-        if(chars[l] != chars[r]){
-            deleted = true;
-            return validPalindrome(chars,l+1,r,deleted) || validPalindrome(chars,l,r-1,deleted);
-        }
-        return validPalindrome(chars,l+1,r-1,deleted);
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--)
+            if (s.charAt(i) != s.charAt(j)) {
+                int i1 = i, j1 = j - 1, i2 = i + 1, j2 = j;
+                while (i1 < j1 && s.charAt(i1) == s.charAt(j1)) {i1++; j1--;};
+                while (i2 < j2 && s.charAt(i2) == s.charAt(j2)) {i2++; j2--;};
+                return i1 >= j1 || i2 >= j2;
+            }
+        return true;
     }
 }
 // @lc code=end
