e1a1b10b44364d5d5becde74d47494b57092a8e1
ZhangTie <zhangtie@hust.edu.cn>
2020-03-31 13:42:56 +0800

    一次遍历/循环体优化

--- 524.通过删除字母匹配到字典里最长单词.java
+++ 524.通过删除字母匹配到字典里最长单词.java
@@ -7,31 +7,17 @@
 // @lc code=start
 class Solution {
     public String findLongestWord(String s, List<String> d) {
-        ArrayList<String> ans = new ArrayList<>();
-        for(String word : d)
-            if(isSubstring(s,word))
-                ans.add(word);
-        int length = 0;
-        String longestAns = "";
-        for(String word : ans)
-            if(word.length() > length){
-                length = word.length();
-                longestAns = word;
-            }else if(word.length() == length){
-                if(word.compareTo(longestAns) < 0)
-                    longestAns = word;
-            }
-        return longestAns;
-    }
-
-    private boolean isSubstring(String s, String word){
-        int i = 0, j = 0, length1 = s.length(), length2 = word.length();
-        while(i < length1 && j < length2){
-            if(s.charAt(i) == word.charAt(j))
-                j++;
-            i++;
+        String ans = "";
+        for(String word : d){
+            int length1 = ans.length(), length2 = word.length();
+            if(length1 > length2) continue;
+            int i = 0;
+            for(char c : s.toCharArray())
+                if(i < length2 && c == word.charAt(i)) i++;            
+            if(i == length2 && (length1 < length2 || word.compareTo(ans) < 0))
+                ans = word;
         }
-        return j == length2;
+        return ans;
     }
 }
 // @lc code=end
