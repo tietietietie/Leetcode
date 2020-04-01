15523326cb1fb08c01034d7a309a1751ee34c820
ZhangTie <zhangtie@hust.edu.cn>
2020-04-01 17:00:48 +0800

    投票问题

--- 169.多数元素.java
+++ 169.多数元素.java
@@ -7,20 +7,18 @@
 // @lc code=start
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int majority = nums[0];
-        for(int i : nums){
-            if(map.get(i) == null){
-                map.put(i,1);
-            }else{
-                map.put(i,map.get(i)+1);
-                if(map.get(i) > map.get(majority))
-                    majority = i;
+        int count = 1, majority = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            if(count == 0){
+                count = 1;
+                majority = nums[i];
             }
-            if(map.get(majority) > nums.length/2)
-                return majority;
+            else if(nums[i] == majority)
+                count++;
+            else
+                count--;
         }
-        return -1;
+        return majority;
     }
 }
 // @lc code=end
