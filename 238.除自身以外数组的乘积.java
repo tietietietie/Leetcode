f8fd63a90507e07686dcb6ca41cfff10d449dac6
ZhangTie <zhangtie@hust.edu.cn>
2020-04-02 16:13:51 +0800

    不适用额外空间

--- 238.除自身以外数组的乘积.java
+++ 238.除自身以外数组的乘积.java
@@ -7,17 +7,15 @@
 // @lc code=start
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] l = new int[nums.length];
-        int[] r = new int[nums.length];
         int[] ans = new int[nums.length];
-        l[0] = 1;
-        r[nums.length-1] = 1;
+        ans[0] = 1;
         for(int i = 1; i < nums.length; i++)
-            l[i] = l[i-1]*nums[i-1];
-        for(int i = nums.length-2; i >= 0; i--)
-            r[i] = r[i+1]*nums[i+1];
-        for(int i = 0; i < nums.length; i++)
-            ans[i] = l[i]*r[i];
+            ans[i] = ans[i-1]*nums[i-1];
+        int right = 1;
+        for(int i = nums.length-1; i>= 0; i--){
+            ans[i] = ans[i]*right;
+            right *= nums[i];
+        }
         return ans;
     }
 }
