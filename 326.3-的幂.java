6ff6e2d7829aaa0cfdfa7809367740629cb552c0
ZhangTie <zhangtie@hust.edu.cn>
2020-04-02 15:30:26 +0800

    int限制位数

--- 326.3-的幂.java
+++ 326.3-的幂.java
@@ -7,11 +7,7 @@
 // @lc code=start
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n == 0) return false;
-        while(n%3 == 0){
-            n /= 3;
-        }
-        return n == 1 ? true : false;
+        return n > 0 && (1162261467 % n == 0);
     }
 }
 // @lc code=end
