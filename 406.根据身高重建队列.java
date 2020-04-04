02b2f6740575c155ed2dcdcf59279ab3174a88eb
ZhangTie <zhangtie@hust.edu.cn>
2020-04-04 09:58:43 +0800

    linkedlist

--- 406.根据身高重建队列.java
+++ 406.根据身高重建队列.java
@@ -8,7 +8,7 @@
 class Solution {
     public int[][] reconstructQueue(int[][] people) {
         Arrays.sort(people,(o1,o2) -> o1[0] == o2[0] ? o1[1]-o2[1] : o2[0]-o1[0]);
-        ArrayList<int[]> queue = new ArrayList<>();
+        LinkedList<int[]> queue = new LinkedList<>();
         for(int[] p : people)
             queue.add(p[1],p);
         return queue.toArray(new int[queue.size()][]);
