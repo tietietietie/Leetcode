6976e4ccb91d75879f63fc7bd53da85be443f918
ZhangTie <zhangtie@hust.edu.cn>
2020-04-04 09:21:57 +0800

    普通方法创建Comparator

--- 435.无重叠区间.java
+++ 435.无重叠区间.java
@@ -8,7 +8,12 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         if(intervals.length == 0) return 0;
-        Arrays.sort(intervals,(o1,o2) -> o1[1]-o2[1]);
+        Arrays.sort(intervals,new Comparator<int[]>(){
+            @Override
+            public int compare(int[] o1, int[] o2){
+                return o1[1] - o2[1];
+            }
+        });
         int count = 1;
         int curEnd = intervals[0][1];
         for(int i = 1; i < intervals.length; i++)
