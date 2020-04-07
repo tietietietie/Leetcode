67469eb0385297c5b3a9f0d80a47b2b866585ae0
ZhangTie <zhangtie@hust.edu.cn>
2020-04-07 09:54:00 +0800

    下标数组/二分

--- 392.判断子序列.java
+++ 392.判断子序列.java
@@ -7,15 +7,22 @@
 // @lc code=start
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        if(s.equals("")) return true;
-        if(t.equals("")) return false;
-        int i = 0, j = 0;
-        while(i < s.length() && j < t.length()){
-            if(s.charAt(i) == t.charAt(j))
-                i++;
-            j++;
+        List<Integer>[] idx = new List[256]; // Just for clarity
+        for (int i = 0; i < t.length(); i++) {
+            if (idx[t.charAt(i)] == null)
+                idx[t.charAt(i)] = new ArrayList<>();
+            idx[t.charAt(i)].add(i);
         }
-        return i == s.length();
+        
+        int prev = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (idx[s.charAt(i)] == null) return false; // Note: char of S does NOT exist in T causing NPE
+            int j = Collections.binarySearch(idx[s.charAt(i)], prev);
+            if (j < 0) j = -j-1;
+            if (j == idx[s.charAt(i)].size()) return false;
+            prev = idx[s.charAt(i)].get(j) + 1;
+        }
+        return true;
     }
 }
 // @lc code=end
