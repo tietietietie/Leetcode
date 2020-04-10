6ed1515f1c04202e96c693927778378e0cce9b75
ZhangTie <zhangtie@hust.edu.cn>
2020-04-10 16:49:54 +0800

    贪心枚举+BFS（用arraylist存储平方数）

--- 279.完全平方数.java
+++ 279.完全平方数.java
@@ -7,7 +7,7 @@
 // @lc code=start
 class Solution {
     public int numSquares(int n) {
-        HashSet<Integer> squareNums = new HashSet<>();
+        ArrayList<Integer> squareNums = new ArrayList<>();
         for(int i = 1; i*i <= n; i++)
             squareNums.add(i*i);
         HashSet<Integer> curLevel = new HashSet<>();
@@ -17,11 +17,14 @@ class Solution {
             level++;
             HashSet<Integer> nextLevel = new HashSet<>();
             for(int remainder : curLevel){
-                if(squareNums.contains(remainder))
-                    return level;
-                for(int squareNum : squareNums)
-                    if(remainder > squareNum)
-                        nextLevel.add(remainder-squareNum);
+                for(int squareNum : squareNums){
+                    if(remainder == squareNum)
+                        return level;
+                    else if(remainder > squareNum)
+                        nextLevel.add(remainder - squareNum);
+                    else
+                        break;
+                }
             }
             curLevel = nextLevel;
         }
