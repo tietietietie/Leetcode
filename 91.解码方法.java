84aef980c4f386c4e8416d39a92f088f0aab93f9
ZhangTie <zhangtie@hust.edu.cn>
2020-04-10 19:46:48 +0800

    DP(改成char数组减了一秒)

--- 91.解码方法.java
+++ 91.解码方法.java
@@ -8,17 +8,18 @@
 class Solution {
     public int numDecodings(String s) {
         int n = s.length();
+        char[] chars = s.toCharArray();
         int[] dp = new int[n+1];
-        if(s.charAt(0) == '0') return 0;
+        if(chars[0] == '0') return 0;
         dp[0] = 1;
         dp[1] = 1;
         for(int i = 2; i <= n; i++){
-            if(s.charAt(i-1) == '0'){
-                if(s.charAt(i-2) == '1' || s.charAt(i-2) == '2')
+            if(chars[i-1] == '0'){
+                if(chars[i-2] == '1' || chars[i-2] == '2')
                     dp[i] = dp[i-2];
                 else
                     return 0;
-            }else if(s.charAt(i-2) == '1' || (s.charAt(i-2) == '2' && s.charAt(i-1) >= '1' && s.charAt(i-1) <= '6'))
+            }else if(chars[i-2] == '1' || (chars[i-2] == '2' && chars[i-1] >= '1' && chars[i-1] <= '6'))
                 dp[i] = dp[i-1]+dp[i-2];
             else
                 dp[i] = dp[i-1];
