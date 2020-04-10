d99913b4fe0fde913cff5de9eb2e6893d346e5df
ZhangTie <zhangtie@hust.edu.cn>
2020-04-10 14:40:12 +0800

    贪心

--- 343.整数拆分.java
+++ 343.整数拆分.java
@@ -7,16 +7,11 @@
 // @lc code=start
 class Solution {
     public int integerBreak(int n) {
-        int[] dp = new int[n+1];
-        dp[1] = 0;
-        dp[2] = 1;
-        for(int i = 3; i <= n; i++){
-            int max = 0;
-            for(int j = 1; j <= i/2; j++)
-                max = Math.max(max,Math.max(j,dp[j])*Math.max(i-j,dp[i-j]));
-            dp[i] = max;
-        }
-        return dp[n];
+        if(n <= 3) return n-1;
+        int a = n/3, b = n%3;
+        if(b == 0) return (int)Math.pow(3,a);
+        if(b == 1) return (int)Math.pow(3,a-1) * 4;
+        return (int)Math.pow(3,a) * 2;
     }
 }
 // @lc code=end
