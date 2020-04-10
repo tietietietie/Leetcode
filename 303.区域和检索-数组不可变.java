d98da32d74b01e754493448ea9129c861b75101d
ZhangTie <zhangtie@hust.edu.cn>
2020-04-10 14:04:57 +0800

    前缀和（简化版）

--- 303.区域和检索-数组不可变.java
+++ 303.区域和检索-数组不可变.java
@@ -9,16 +9,13 @@ class NumArray {
     public int[] preSum;
 
     public NumArray(int[] nums) {
-        preSum = new int[nums.length];
-        if(nums.length == 0) return;
-        preSum[0] = nums[0];
-        for(int i = 1; i < nums.length; i++)
-            preSum[i] = preSum[i-1]+nums[i]; 
+        preSum = new int[nums.length+1];
+        for(int i = 0; i < nums.length; i++)
+            preSum[i+1] = preSum[i]+nums[i]; 
     }
     
     public int sumRange(int i, int j) {
-        if(i == 0) return preSum[j];
-        return preSum[j]-preSum[i-1];
+        return preSum[j+1]-preSum[i];
     }
 }
 
