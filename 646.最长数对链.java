9770ccde84cdc036547d548afe08048277e780fc
ZhangTie <zhangtie@hust.edu.cn>
2020-04-11 14:57:57 +0800

    贪心

--- 646.最长数对链.java
+++ 646.最长数对链.java
@@ -8,17 +8,12 @@
 class Solution {
     public int findLongestChain(int[][] pairs) {
         Arrays.sort(pairs,(o1,o2) -> o1[1] - o2[1]);
-        int[] dp = new int[pairs.length];
-        Arrays.fill(dp,1);
-        int ans = 1;
-        for(int i = 0; i < pairs.length; i++){
-            for(int j = 0; j < i; j++){
-                if(pairs[j][1] < pairs[i][0])
-                    dp[i] = Math.max(dp[i],dp[j]+1);
-                else
-                    break;
+        int ans = 1, curEnd = pairs[0][1];
+        for(int i = 1; i < pairs.length; i++){
+            if(pairs[i][0] > curEnd){
+                curEnd = pairs[i][1];
+                ans++;
             }
-            ans = Math.max(dp[i],ans); 
         }
         return ans;
     }
