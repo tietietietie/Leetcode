ce1076c5fa3e9479d7a0b375265888cee02bc65c
ZhangTie <zhangtie@hust.edu.cn>
2020-04-13 16:10:35 +0800

    BF/DFS

--- 494.目标和.java
+++ 494.目标和.java
@@ -7,17 +7,16 @@
 // @lc code=start
 class Solution {
     public int findTargetSumWays(int[] nums, int S) {
-        int[][] dp = new int[nums.length][2001];
-        dp[0][nums[0]+1000] = 1;
-        dp[0][-nums[0]+1000] += 1;
-        for(int i = 1; i < nums.length; i++)
-            for(int j = -1000; j <= 1000; j++){
-                if(j-nums[i] >= -1000)
-                    dp[i][j+1000] += dp[i-1][j-nums[i]+1000];
-                if(j+nums[i] <= 1000)
-                    dp[i][j+1000] += dp[i-1][j+nums[i]+1000];
-            }
-        return S > 1000 ? 0 : dp[nums.length-1][S+1000];
+        return dfs(nums,0,S);
+    }
+
+    private int dfs(int[] nums, int i, int target){
+        if(i == nums.length && target == 0) return 1;
+        if(i == nums.length) return 0;
+        int ans = 0;
+        ans += dfs(nums,i+1,target-nums[i]);
+        ans += dfs(nums,i+1,target+nums[i]);
+        return ans;
     }
 }
 // @lc code=end
