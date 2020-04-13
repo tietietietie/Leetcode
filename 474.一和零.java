424eefa2aba4f9d3d5ba193ef1e9548ac7fcd70b
ZhangTie <zhangtie@hust.edu.cn>
2020-04-13 17:38:50 +0800

    DP/01背包

--- 474.一和零.java
+++ 474.一和零.java
@@ -7,27 +7,24 @@
 // @lc code=start
 class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
-        int[][][] dp = new int[strs.length+1][m+1][n+1];
-        for(int i = 1; i <= strs.length; i++){
-            int[] count = countStr(strs[i-1]);
-            for(int j = 0; j <= m; j++)
-                for(int k = 0; k <= n; k++){
-                    if(j >= count[0] && k >= count[1])
-                        dp[i][j][k] = Math.max(dp[i-1][j][k],dp[i-1][j-count[0]][k-count[1]]+1);
-                    else
-                        dp[i][j][k] = dp[i-1][j][k];
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 0; i < strs.length; i++){
+            int[] count = countStr(strs[i]);
+            for(int j = m; j >= count[0]; j--)
+                for(int k = n; k >= count[1]; k--){
+                    dp[j][k] = Math.max(dp[j][k],dp[j-count[0]][k-count[1]]+1);
                 }
         }  
-        return dp[strs.length][m][n];
+        return dp[m][n];
     }
 
     private int[] countStr(String s){
         int[] count = new int[2];
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == '0')
-                count[0] = count[0]+1;
+                count[0] += 1;
             else
-                count[1] = count[1]+1;
+                count[1] += 1;
         }
         return count;
     }
