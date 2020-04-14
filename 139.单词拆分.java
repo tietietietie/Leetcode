1cb74ce8ffe4762506707f5a9340dee5d3d6c776
ZhangTie <zhangtie@hust.edu.cn>
2020-04-14 17:51:01 +0800

    完全背包

--- 139.单词拆分.java
+++ 139.单词拆分.java
@@ -7,15 +7,14 @@
 // @lc code=start
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        HashSet<String> set = new HashSet<>(wordDict);
         boolean[] dp = new boolean[s.length()+1];
         dp[0] = true;
         for(int i = 1; i <= s.length(); i++)
-            for(int j = 0; j < i; j++)
-                if(dp[j] && set.contains(s.substring(j,i))){
-                    dp[i] = true;
-                    break;
-                }
+            for(String word : wordDict){
+                int wordLen = word.length();
+                if(wordLen <= i && word.equals(s.substring(i-wordLen,i)))
+                    dp[i] = dp[i-wordLen] || dp[i];
+            }
         return dp[s.length()];
     }
 }
