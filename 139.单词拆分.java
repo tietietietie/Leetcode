869e83d6c97afb1d582f62ca35b50739ac5bdc43
ZhangTie <zhangtie@hust.edu.cn>
2020-04-14 16:59:54 +0800

    记忆优化递归

--- 139.单词拆分.java
+++ 139.单词拆分.java
@@ -6,15 +6,21 @@
 
 // @lc code=start
 class Solution {
+    public Boolean[] memo;
     public boolean wordBreak(String s, List<String> wordDict) {
+        memo = new Boolean[s.length()+1];
         return wordBreak(s,new HashSet<String>(wordDict), 0);
     }
     private boolean wordBreak(String s, HashSet<String> set, int start){
-        if(start == s.length()) return true;
+        if(start == s.length()){
+            return memo[s.length()] = true;
+        }
+        if(memo[start] != null)
+            return memo[start];
         for(int end = start+1; end <= s.length(); end++)
             if(set.contains(s.substring(start,end)) && wordBreak(s,set,end))
-                return true;
-        return false;
+                return memo[start] = true;
+        return memo[start] = false;
     }
 }
 // @lc code=end
