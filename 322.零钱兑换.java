786866b3a0b8597e85ec8707160640a03e328e3a
ZhangTie <zhangtie@hust.edu.cn>
2020-04-14 11:15:11 +0800

    一维DP

--- 322.零钱兑换.java
+++ 322.零钱兑换.java
@@ -7,24 +7,14 @@
 // @lc code=start
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[][] dp = new int[coins.length+1][amount+1];
-        Arrays.fill(dp[0],-1);
-        for(int i = 1; i <= coins.length; i++)
-            dp[i][0] = 0;
-        for(int i = 1; i <= coins.length; i++)
-            for(int j = 1; j <= amount; j++){
-                if(j < coins[i-1])
-                    dp[i][j] = dp[i-1][j];
-                else if(dp[i][j-coins[i-1]] != -1 && dp[i-1][j] != -1)
-                    dp[i][j] = Math.min(dp[i-1][j],dp[i][j-coins[i-1]]+1);
-                else if(dp[i][j-coins[i-1]] != -1)
-                    dp[i][j] = dp[i][j-coins[i-1]]+1;
-                else if(dp[i-1][j] != -1)
-                    dp[i][j] = dp[i-1][j];
-                else
-                    dp[i][j] = -1;
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0] = 0;
+        for(int coin : coins)
+            for(int i = coin; i <= amount; i++){
+                dp[i] = Math.min(dp[i],dp[i-coin]+1);
             }
-        return dp[coins.length][amount];
+        return dp[amount] == amount+1 ? -1 : dp[amount];
     }
 }
 // @lc code=end
