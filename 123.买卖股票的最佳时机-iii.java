a90001fb990ad7ae510f18902b808c9278b7a55c
ZhangTie <zhangtie@hust.edu.cn>
2020-04-16 17:31:05 +0800

    dp

---
+++ 123.买卖股票的最佳时机-iii.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=123 lang=java
+ *
+ * [123] 买卖股票的最佳时机 III
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if(n < 2) return 0;
+        int[][][] dp = new int[n+1][3][2];
+        dp[1][2][0] = 0;
+        dp[1][1][1] = -prices[0];
+        dp[1][1][0] = 0;
+        dp[1][0][0] = 0;
+        dp[1][2][1] = -prices[0];
+        dp[1][0][1] = Integer.MIN_VALUE;
+        for(int i = 2; i <= n; i++)
+            for(int k = 1; k < 3; k++){
+                dp[i][k][0] = Math.max(dp[i-1][k][0],dp[i-1][k][1]+prices[i-1]);
+                dp[i][k][1] = Math.max(dp[i-1][k][1],dp[i-1][k-1][0]-prices[i-1]);
+            }
+        return dp[n][2][0];
+    }
+}
+// @lc code=end
+
