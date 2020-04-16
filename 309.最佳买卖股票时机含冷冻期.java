d746edd606ccbac046971f99af4c9b600debc715
ZhangTie <zhangtie@hust.edu.cn>
2020-04-16 15:46:33 +0800

    dp简化版

--- 309.最佳买卖股票时机含冷冻期.java
+++ 309.最佳买卖股票时机含冷冻期.java
@@ -9,16 +9,16 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
         if(n < 2) return 0;
-        int[][] dp = new int[n+1][2];
-        dp[0][0] = 0;
-        dp[0][1] = Integer.MIN_VALUE;
-        dp[1][0] = 0;
-        dp[1][1] = -prices[0];
+        int dp_i_0 = 0;
+        int dp_i_1 = -prices[0];
+        int dp_pre_0 = 0;
         for(int i = 2; i <= n; i++){
-            dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1]+prices[i-1]);
-            dp[i][1] = Math.max(dp[i-1][1],dp[i-2][0]-prices[i-1]);
+            int temp = dp_i_0;
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i-1]);
+            dp_i_1 = Math.max(dp_i_1, dp_pre_0 - prices[i-1]);
+            dp_pre_0 = temp;
         }
-        return dp[n][0];
+        return dp_i_0;
     }
 }
 // @lc code=end
