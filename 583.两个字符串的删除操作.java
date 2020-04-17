c830b6d30f5a9d1a1d509e179924f541e8e540ed
ZhangTie <zhangtie@hust.edu.cn>
2020-04-17 15:55:02 +0800

    一维DP

--- 583.两个字符串的删除操作.java
+++ 583.两个字符串的删除操作.java
@@ -8,20 +8,24 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
-        int[][] dp = new int[m+1][n+1];
-        for(int i = 0; i <= m; i++)
+        int[] dp = new int[n+1];
+        for(int i = 0; i <= m; i++){
+            int[] temp = new int[n+1];
             for(int j = 0; j <= n; j++){
                 if(i == 0 || j == 0){
-                    dp[i][j] = i+j;
+                    temp[j] = i+j;
                     continue;
                 }
                 if(word1.charAt(i-1) == word2.charAt(j-1))
-                    dp[i][j] = dp[i-1][j-1];
+                    temp[j] = dp[j-1];
                 else{
-                    dp[i][j] = Math.min(dp[i][j-1]+1,dp[i-1][j]+1);
+                    temp[j] = Math.min(temp[j-1]+1,dp[j]+1);
                 }
             }
-        return dp[m][n];
+            dp = temp;
+        }
+            
+        return dp[n];
     }
 }
 // @lc code=end
