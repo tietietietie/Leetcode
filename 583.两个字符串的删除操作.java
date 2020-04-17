b66f23f052a4ab7fa2b2bb241b81c6cf98a3163b
ZhangTie <zhangtie@hust.edu.cn>
2020-04-17 16:10:54 +0800

    LCS/DP

--- 583.两个字符串的删除操作.java
+++ 583.两个字符串的删除操作.java
@@ -8,17 +8,19 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
-        int[][] memo = new int[m+1][n+1];
-        return m + n - 2*lcs(word1,word2,m,n,memo);
-    }
-    private int lcs(String s1, String s2, int m, int n, int[][] memo){
-        if(m == 0 || n == 0) return 0;
-        if(memo[m][n] > 0) return memo[m][n];
-        if(s1.charAt(m-1) == s2.charAt(n-1))
-            memo[m][n] = 1 + lcs(s1,s2,m-1,n-1,memo);
-        else
-            memo[m][n] = Math.max(lcs(s1,s2,m-1,n,memo),lcs(s1,s2,m,n-1,memo));
-        return memo[m][n];
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 0; i <= m; i++)
+            for(int j = 0; j <= n; j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if(word1.charAt(i-1) == word2.charAt(j-1))
+                    dp[i][j] = 1+dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        return m + n - 2*dp[m][n];
     }
 }
 // @lc code=end
