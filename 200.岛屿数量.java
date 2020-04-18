a820787d424415a733138c61acb91235040f05a1
ZhangTie <zhangtie@hust.edu.cn>
2020-04-18 21:57:54 +0800

    dfs

---
+++ 200.岛屿数量.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=200 lang=java
+ *
+ * [200] 岛屿数量
+ */
+
+// @lc code=start
+class Solution {
+    public int numIslands(char[][] grid) {
+        int m = grid.length;
+        if(m == 0) return 0;
+        int n = grid[0].length;
+        if(n == 0) return 0;
+        int ans = 0;
+        for(int i = 0; i < m; i++)
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
+                    dfs(grid,m,n,i,j);
+                    ans++;
+                }    
+            }
+        return ans;
+    }
+    private void dfs(char[][] grid, int m, int n, int i, int j){
+        if(i == m || i < 0 || j == n || j < 0) return;
+        if(grid[i][j] == '0' || grid[i][j] == '2') return;
+        grid[i][j] = '2';
+        dfs(grid,m,n,i-1,j);
+        dfs(grid,m,n,i+1,j);
+        dfs(grid,m,n,i,j-1);
+        dfs(grid,m,n,i,j+1);
+        return;
+    }
+}
+// @lc code=end
+
