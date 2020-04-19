00f0679a5d9a198c170637052a67c94d184f4069
ZhangTie <zhangtie@hust.edu.cn>
2020-04-19 14:17:33 +0800

    无向图DFS

--- 547.朋友圈.java
+++ 547.朋友圈.java
@@ -8,53 +8,20 @@
 class Solution {
     public int findCircleNum(int[][] M) {
         int m = M.length;
-        UF uf = new UF(m);
+        boolean[] visited = new boolean[m];
+        int ans = 0;
         for(int i = 0; i < m; i++)
-            for(int j = i+1; j < m; j++)
-                if(M[i][j] == 1)
-                    uf.union(i,j);
-        return uf.count();
+            if(!visited[i]){
+                ans++;
+                dfs(M,visited,m,i);
+            }
+        return ans;
     }
-
-    private class UF{
-        private int[] parent;
-        private int[] size;
-        private int count;
-        public UF(int n){
-            parent = new int[n];
-            size = new int[n];
-            count = n;
-            for(int i = 0; i < n; i++){
-                parent[i] = i;
-                size[i] = 1;
-            }
-        }
-        public int find(int x){
-            if(parent[x] == x)
-                return x;
-            return find(parent[x]);
-        }
-        public void union(int x, int y){
-            int xRoot = find(x);
-            int yRoot = find(y);
-            if(xRoot == yRoot) return;
-            if(size[xRoot] > size[yRoot]){
-                parent[yRoot] = xRoot;
-                size[xRoot] += size[yRoot];
-            }else{
-                parent[xRoot] = yRoot;
-                size[yRoot] += size[xRoot];
-            }
-            count--;
-        }
-        public boolean connected(int x, int y){
-            int xRoot = find(x);
-            int yRoot = find(y);
-            return xRoot == yRoot;
-        }
-        public int count(){
-            return count;
-        }
+    private void dfs(int[][] M, boolean[] visited, int m, int i){
+        visited[i] = true;
+        for(int j = 0; j < m; j++)
+            if(M[i][j] == 1 && !visited[j])
+                dfs(M,visited,m,j);
     }
 }
 // @lc code=end
