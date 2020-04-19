0e7fbfadb75c9eea539e21f21973a3e7af532f12
ZhangTie <zhangtie@hust.edu.cn>
2020-04-19 13:20:28 +0800

    DFS

--- 695.岛屿的最大面积.java
+++ 695.岛屿的最大面积.java
@@ -11,7 +11,7 @@ class Solution {
         int m = grid.length, n = grid[0].length;
         for(int i = 0; i < m; i++)
             for(int j = 0; j < n; j++){
-                if(grid[i][j] == 0 || grid[i][j] == -1)
+                if(grid[i][j] == 0)
                     continue;
                 else
                     ans = Math.max(ans,dfs(grid,m,n,i,j));
@@ -20,9 +20,8 @@ class Solution {
     }
 
     private int dfs(int[][] grid, int m, int n, int i, int j){
-        if(i == m || j == n || i < 0 || j < 0) return 0;
-        if(grid[i][j] == 0 || grid[i][j] == -1) return 0;
-        grid[i][j] = -1;
+        if(i == m || j == n || i < 0 || j < 0 || grid[i][j] == 0) return 0;
+        grid[i][j] = 0;
         return 1 + dfs(grid,m,n,i-1,j) + dfs(grid,m,n,i+1,j) + dfs(grid,m,n,i,j-1) + dfs(grid,m,n,i,j+1);
     }
 }
