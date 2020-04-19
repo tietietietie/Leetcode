5abd13dc3ee991f07cc0d3c2b955a20359662065
ZhangTie <zhangtie@hust.edu.cn>
2020-04-19 14:07:31 +0800

    UF

--- 547.朋友圈.java
+++ 547.朋友圈.java
@@ -7,25 +7,54 @@
 // @lc code=start
 class Solution {
     public int findCircleNum(int[][] M) {
-        int n = M.length;
-        int[] uf = new int[n];
-        for(int i = 0; i < n; i++)
-            uf[i] = i;
-        for(int i = 0; i < n; i++)
-            for(int j = i+1; j < n; j++){
-                if(M[i][j] == 1 && uf[i] != uf[j]){
-                    int temp = uf[j];
-                    for(int k = 0; k < n; k++){
-                        if(uf[k] == temp)
-                            uf[k] = uf[i];
-                    }
-                }
+        int m = M.length;
+        UF uf = new UF(m);
+        for(int i = 0; i < m; i++)
+            for(int j = i+1; j < m; j++)
+                if(M[i][j] == 1)
+                    uf.union(i,j);
+        return uf.count();
+    }
+
+    private class UF{
+        private int[] parent;
+        private int[] size;
+        private int count;
+        public UF(int n){
+            parent = new int[n];
+            size = new int[n];
+            count = n;
+            for(int i = 0; i < n; i++){
+                parent[i] = i;
+                size[i] = 1;
             }
-        HashSet<Integer> set = new HashSet<>();
-        for(int item : uf)
-            if(!set.contains(item))
-                set.add(item);
-        return set.size();
+        }
+        public int find(int x){
+            if(parent[x] == x)
+                return x;
+            return find(parent[x]);
+        }
+        public void union(int x, int y){
+            int xRoot = find(x);
+            int yRoot = find(y);
+            if(xRoot == yRoot) return;
+            if(size[xRoot] > size[yRoot]){
+                parent[yRoot] = xRoot;
+                size[xRoot] += size[yRoot];
+            }else{
+                parent[xRoot] = yRoot;
+                size[yRoot] += size[xRoot];
+            }
+            count--;
+        }
+        public boolean connected(int x, int y){
+            int xRoot = find(x);
+            int yRoot = find(y);
+            return xRoot == yRoot;
+        }
+        public int count(){
+            return count;
+        }
     }
 }
 // @lc code=end
