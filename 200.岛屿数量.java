8fae2c7a49a726f46fd8847558595e8956243c13
ZhangTie <zhangtie@hust.edu.cn>
2020-04-19 13:21:27 +0800

    DFS

--- 200.岛屿数量.java
+++ 200.岛屿数量.java
@@ -23,8 +23,8 @@ class Solution {
     }
     private void dfs(char[][] grid, int m, int n, int i, int j){
         if(i == m || i < 0 || j == n || j < 0) return;
-        if(grid[i][j] == '0' || grid[i][j] == '2') return;
-        grid[i][j] = '2';
+        if(grid[i][j] == '0') return;
+        grid[i][j] = '0';
         dfs(grid,m,n,i-1,j);
         dfs(grid,m,n,i+1,j);
         dfs(grid,m,n,i,j-1);
