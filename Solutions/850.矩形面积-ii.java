1cd2fd713ae3635cd20eddc62e845abbdbc9468d
ZhangTie <zhangtie@hust.edu.cn>
2020-05-08 20:29:19 +0800

    线段树

--- Solutions/850.矩形面积-ii.java
+++ Solutions/850.矩形面积-ii.java
@@ -23,22 +23,54 @@ class Solution {
         HashMap<Integer,Integer> xmapi = new HashMap<>();
         for(i = 0; i < imapx.length; i++)
             xmapi.put(imapx[i],i);
-        int[] count = new int[imapx.length];
+        Node active = new Node(0,imapx.length,imapx);
         int cur_y = events[0][0];
         long ans = 0;
         for(int[] event : events){
             int y = event[0], flag = event[1], x1 = xmapi.get(event[2]), x2 = xmapi.get(event[3]);
-            int query = 0;
-            for(i = 0; i < count.length; i++)
-                if(count[i] > 0)
-                    query += imapx[i+1] - imapx[i];
+            int query = active.getTotal();
             ans += (long)query * (y - cur_y);
-            for(i = x1; i < x2; i++)
-                count[i] += flag;
+            active.update(x1,x2,flag);
             cur_y = y;
         }
         ans %= 1000000007;
         return (int) ans;
+    }
+
+    private class Node{
+        private int start;
+        private int end;
+        private Integer[] imapx;
+        private int count;
+        private int total;
+        private Node left;
+        private Node right;
+        public Node(int start, int end, Integer[] imapx){
+            this.start = start;
+            this.end = end;
+            this.imapx = imapx;
+            this.count = 0;
+            this.total = 0;
+            this.left = null;
+            this.right = null;
+        }
+        public int getTotal(){
+            return this.total;
+        }
+        public void update(int l, int r, int flag){
+            if(l >= r) return;
+            if(l == start && r == end)
+                count += flag;
+            else{
+                int mid = start + (end - start) / 2;
+                if(left == null) left = new Node(start,mid,imapx);
+                if(right == null) right = new Node(mid,end,imapx);
+                left.update(l,Math.min(mid,r),flag);
+                right.update(Math.max(l,mid),r,flag);
+            }
+            if(count > 0) total = imapx[end] - imapx[start];
+            else total = (left == null ? 0 : left.getTotal()) + (right == null ? 0 : right.getTotal());
+        }
     }
 }
 // @lc code=end
